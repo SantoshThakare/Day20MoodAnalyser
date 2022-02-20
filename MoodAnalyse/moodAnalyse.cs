70e9bf3330d6b16f4aa191b1a4dc3f5620c22b06
SantoshThakare <santoshthakare50@gmail.com>
2022-02-20 18:16:01 +0530

    User Mood

---
+++ MoodAnalyse/moodAnalyse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyse
+{
+    public class moodAnalyse
+    {
+        string message;
+        public moodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (message.ToLower().Contains("happy"))
+           {
+                return "happy";
+                
+            }
+            else
+            {
+                return "sad";
+
+            }
+        }
+    }
+}
